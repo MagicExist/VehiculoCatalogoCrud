f1fce22aea49cf1e5f31f5a3652cfbcbc4d3219f
johhan sneyder <Johhanparra1@gmail.com>
2023-10-25 11:32:38 -0500

    ConnectionDB method added

---
+++ VehiculoCatalogoCrud/Model/ConnectionDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+
+namespace VehiculoCatalogoCrud.Model
+{
+    internal class ConnectionDB
+    {
+        private SqlConnection _connection;
+        private string _exceptionMessage;
+
+        public ConnectionDB()
+        {
+        
+        }
+
+        public void openConnection() 
+        {
+            try
+            {
+                string stringConnection = ConfigurationManager.ConnectionStrings["StrConnection"].ToString();
+
+                _connection = new SqlConnection(stringConnection);
+                _connection.Open();
+            }
+            catch (Exception ex)
+            {
+                this._exceptionMessage = ex.Message;
+            }
+        }
+    }
+}
