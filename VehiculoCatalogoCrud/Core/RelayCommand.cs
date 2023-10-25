6ba96855c23423bdd29b639259e3a7383a634f38
johhan sneyder <Johhanparra1@gmail.com>
2023-10-25 11:23:19 -0500

    RelayCommand Added

---
+++ VehiculoCatalogoCrud/Core/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VehiculoCatalogoCrud.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        protected Action<object> _execute;
+        protected Func<object,bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+
+        public RelayCommand(Action<object> execute, Func<object,bool> canExecute)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute)
+        {
+            this._execute = execute;
+            this._canExecute = null;
+        }
+
+        public virtual bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
